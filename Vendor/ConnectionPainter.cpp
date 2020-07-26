bce9791fcbd731bc8cb1bb74e696cde63fd5793b
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2020-07-26 16:59:30 -0400

    Linking the library

---
+++ Vendor/ConnectionPainter.cpp
@@ -0,0 +1,6 @@
+#include "ConnectionPainter.hpp"
+
+ConnectionPainter::ConnectionPainter()
+{
+
+}
---
+++ Vendor/ConnectionPainter.hpp
@@ -0,0 +1,11 @@
+#ifndef CONNECTIONPAINTER_HPP
+#define CONNECTIONPAINTER_HPP
+
+
+class ConnectionPainter
+{
+public:
+    ConnectionPainter();
+};
+
+#endif // CONNECTIONPAINTER_HPP
